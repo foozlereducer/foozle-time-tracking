095829aea98b844859c123faf124d193d8f7668c
Steve Browning <foozlereducers@gmail.com>
2023-01-04 10:43:48 -0500

    Added a new query that still needs to be adapted

---
+++ src/getUsersQuery.ts
@@ -0,0 +1,64 @@
+interface Deployment {
+    users: () => {}.
+    {
+        deployedActionPacks:[]
+    }
+    
+}
+
+interface RemindFilter {
+    isNotStarted: () => {},
+    isNotCompleted: () => {},
+}
+
+class LearnReminderService {{
+    /**
+     * @param \App\Enums\ReminderFilter $reminder_filter
+     * @param \App\Models\Deployment deployment
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function getUsersQuery(reminder_filter: RemindFilter, deployment: Deployment)
+    {
+        let users;
+
+        if (reminder_filter.isNotStarted()) {
+            users = notStartedUsersQuery(deployment);
+        } else if ($reminder_filter->isNotCompleted()) {
+            users = $this->notCompletedUsersQuery(deployment);
+        } else {
+            users = deployment->users();
+        }
+
+        return users;
+    }
+
+    /**
+     * @param \App\Models\Deployment $deployment
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+     private function notStartedUsersQuery(deployment:Deployment) 
+     {
+         return deployment.users?.deployedActionPacks;
+     }
+
+     /**
+     * @param \App\Models\Deployment $deployment
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function notCompletedUsersQuery(deployment: Deployment)
+    {
+        return deployment.users()
+            ->whereHas(
+                'deployedActionPacks',
+                function (Builder $builder) use ($deployment) {
+                    $builder
+                        ->where('deployment_id', $deployment->id)
+                        ->where('time_spent', '!=', 0)
+                        ->whereNull('completed_at');
+                }
+            );
+    }
+}
