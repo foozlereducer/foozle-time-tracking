e5b0ee64f22ccf979b7a1f592c7da92c32458da6
Steve Browning <foozlereducers@gmail.com>
2023-03-03 18:06:28 -0500

    Started to configure rollup; more to do. Added index.js to be a central export for all the modules; this is better for rollup. Added the beginning of the Event Binder class with empty class and half done tests' '

---
+++ src/FoozleEventsBinder.test.ts
@@ -0,0 +1,28 @@
+import CreateVideoObj from './CreateVideoObj';
+let CVO: any = null;
+
+beforeEach(() => {
+  CVO = new CreateVideoObj();
+});
+
+afterEach(() => {
+  CVO = new CreateVideoObj();
+});
+
+describe('Create Acto Objs', () => {
+  test('default videoObj should have zeroed values', () => {
+    CVO.setObj();
+    const obj = CVO.createObj();
+    expect(obj).toStrictEqual({ video: { isplaying: false, progress: 0, volume: 0, videoname: '' } });
+    expect(obj.video.isplaying).toBe(false);
+    expect(obj.video.volume).toBe(0);
+    expect(obj.video.progress).toBe(0);
+    expect(obj.video.videoname).toBe('');
+  });
+
+  test('videoObj should return customized isplaying:true, volume: 15 and progress:11.0502', () => {
+    CVO.setObj(true, 15, 11.0502, 'abc');
+    const obj = CVO.createObj();
+    expect(obj).toStrictEqual({ video: { isplaying: true, volume: 15, progress: 11.0502, videoname: 'abc' } });
+  });
+});
