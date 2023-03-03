228c45cf92e1eb14c3442a9410e209fc07038f7c
Steve Browning <foozlereducers@gmail.com>
2023-03-03 18:11:26 -0500

    Added first failing test in the TDD process for the Event Binders

--- src/FoozleEventsBinder.test.ts
+++ src/FoozleEventsBinder.test.ts
@@ -1,28 +1,19 @@
-import CreateVideoObj from './CreateVideoObj';
-let CVO: any = null;
+import { FoozleEventsBinder } from "./FoozleEventsBinder";
+let FEB: any = null;
 
 beforeEach(() => {
-  CVO = new CreateVideoObj();
+  FEB = new FoozleEventsBinder();
 });
 
 afterEach(() => {
-  CVO = new CreateVideoObj();
+  FEB = null;
 });
 
 describe('Create Acto Objs', () => {
-  test('default videoObj should have zeroed values', () => {
-    CVO.setObj();
-    const obj = CVO.createObj();
-    expect(obj).toStrictEqual({ video: { isplaying: false, progress: 0, volume: 0, videoname: '' } });
-    expect(obj.video.isplaying).toBe(false);
-    expect(obj.video.volume).toBe(0);
-    expect(obj.video.progress).toBe(0);
-    expect(obj.video.videoname).toBe('');
-  });
-
-  test('videoObj should return customized isplaying:true, volume: 15 and progress:11.0502', () => {
-    CVO.setObj(true, 15, 11.0502, 'abc');
-    const obj = CVO.createObj();
-    expect(obj).toStrictEqual({ video: { isplaying: true, volume: 15, progress: 11.0502, videoname: 'abc' } });
+  test('should set default bind element for events', () => {
+    
+    const elements = FEB.getElements()
+    expect(elements).toStrictEqual("p, select, button");
+    //expect(obj.video.isplaying).toBe(false);
   });
 });
